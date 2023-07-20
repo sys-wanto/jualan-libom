361629eb3e2f346cf1af8f47d0a0e1f5e81f15c3
sys-wanto <nckdostrow@gmail.com>
2023-07-20 20:22:24 +0700

    currecy

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -292,6 +292,30 @@
             $('#checkoutForm').submit();
         });
 
+        var format = (num) => {
+            var str = num.toString().replace("", ""),
+                parts = false,
+                output = [],
+                i = 1,
+                formatted = null;
+            if (str.indexOf(".") > 0) {
+                parts = str.split(".");
+                str = parts[0];
+            }
+            str = str.split("").reverse();
+            for (var j = 0, len = str.length; j < len; j++) {
+                if (str[j] != ",") {
+                    output.push(str[j]);
+                    if (i % 3 == 0 && j < (len - 1)) {
+                        output.push(",");
+                    }
+                    i++;
+                }
+            }
+            formatted = output.reverse().join("");
+            return ("" + formatted + ((parts) ? "." + parts[1].substr(0, 2) : ""));
+        }
+
         function downpayment() {
             let Percentage = 0;
             let period = $('#instalmentPeriod').find(":selected").val();
@@ -316,11 +340,11 @@
                 instalmentCalc = (price - downPayment) / periodMonth;
                 $('#percentage-calc').text(`${Percentage} %`);
                 $('#down-payment-price-calc').text(
-                    `Down Payment : Rp. ${downPayment}`);
+                    `Down Payment : Rp. ${format(downPayment)}`);
                 $('#tenor-payment-calc').text(
                     `Tenor : ${period}`);
                 $('#instalment-payment-calc').text(
-                    `Tenor : Rp.${Math.round(instalmentCalc)}`);
+                    `Tenor : Rp.${format(Math.round(instalmentCalc))}`);
 
             }
         }
